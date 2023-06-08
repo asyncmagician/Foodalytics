50c33ba3ab70e98477b5816345b18b82df7aad85
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-06-08 14:07:12 +0200

    feat: Send user data to the API through RegisterComponent

--- app/src/app/views/register/register.component.ts
+++ app/src/app/views/register/register.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  registerForm: FormGroup = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  });
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+  ) {}
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const formData = this.registerForm.value;
+      this.http.post('http://localhost:3000/users', formData).subscribe(
+        (response) => {
+          console.log('Envoi réussi :', response);
+        },
+        (error) => {
+          console.error('Erreur lors de l\'envoi du formulaire :', error);
+        }
+      );
+    }
+  }
 }
