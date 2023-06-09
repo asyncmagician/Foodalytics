05fbc04240342c60e51b733a9b05deebc2ee5fa2
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-06-09 16:28:27 +0200

    fix: Pass PUT to CORS

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
         origin: "http://localhost:4200",
-        methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+        methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS", "PUT"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
     });
