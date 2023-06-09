9463f129436c5c6fada07ddd5b4380c46c393a36
Antony Bartolomucci <antony.bartolomucci@stagiairesmns.fr>
2023-06-09 14:43:51 +0200

    fix: Fix Entrypoints for Foods and Categories

--- api/src/modules/categories/categories.module.ts
+++ api/src/modules/categories/categories.module.ts
@@ -7,10 +7,26 @@ import { FoodService } from "../food/food.service";
 import { Food } from "../food/food.entity";
 import { FoodCategoriesService } from "../food-categories/services/food-categories.service";
 import { FoodCategories } from "../food-categories/entities/food-categories.entity";
+import { User } from "../user/user.entity";
+import { JwtModule } from "@nestjs/jwt";
+import { UserService } from "../user/user.service";
+import { JwtStrategy } from "src/strategies/jwt/jwt.strategy";
+import { AuthService } from "../auth/auth.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Categories, Food, FoodCategories])],
+    imports: [TypeOrmModule.forFeature([Categories, Food, FoodCategories, User]),
+    JwtModule.register({
+        secret: "mj4_+L4BxHpqSatv90.96(kN7",
+      }),
+    ],
     controllers: [CategoriesController],
-    providers: [CategoriesService, FoodService, FoodCategoriesService],
+    providers: [
+        CategoriesService, 
+        FoodService, 
+        FoodCategoriesService,
+        UserService,
+        JwtStrategy,
+        AuthService
+    ],
 })
 export class CategoriesModule {}
